e303e7fd56cea6d9260086bcd5f43098d1e0fa43
maxroy80 <maxroy80@gmail.com>
2022-07-11 16:52:04 +0300

    optimize code

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
-let h;
-while ((h = prompt('введите высоту треугольника (целое число)')) != parseInt(h) || h == 0) {
+let height;
+while ((height = prompt('введите высоту треугольника (целое число)')) != parseInt(height) || height == 0) {
     alert('это дожно быть целое число <>0, попробуйте ещё');
 }
-console.log('высота = ' + h);
+console.log('высота = ' + height);
 let triangle = '';
-function moreZero () {
+let h = Math.abs(height);
     for (let y = 0; y < h; y++) {
         for (let x = 1; x <= (2 * h - 1); x++) {
             if (x < (h - y) || x > (2 * h - (h - y))) {
-                triangle = triangle + '.'
+                if (height > 0) triangle = triangle + '.';
+                else triangle = '.' + triangle;
             } else {
-                triangle = triangle + '#';
+                if (height > 0) triangle = triangle + '#';
+                else triangle = '#' + triangle;
             }
         }
-        triangle = triangle + '\n';
+        if ((y < h - 1) && height < 0) triangle = '\n' + triangle;
+        else triangle = triangle + '\n';
     }
-}
-function subZero () {
-    for (let y = 0; -y > h; y++) {
-        for (let x = 1; x <= (-2 * h - 1); x++) {
-            if (x < y + 1 || x > -2 * h - y - 1) {
-                triangle = triangle + '.'
-            } else {
-                triangle = triangle + '#';
-            }
-        }
-        triangle = triangle + '\n';
-    }
-}
-if (h > 0) {moreZero();}
-else {subZero()}
 console.log(triangle);
